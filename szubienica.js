2f9f8da7da1fe2b03ac0418a0907781063b653aa
LukaszStusio <lukasz.stusio@gmail.com>
2017-10-22 16:26:28 +0200

    win/lose and sounds, work finished

--- szubienica.js
+++ szubienica.js
@@ -4,6 +4,10 @@ haslo = haslo.toUpperCase();
 
 
 var dlugosc = haslo.length;
+var ile_skuch = 0;
+
+var yes = new Audio("yes.wav");
+var no = new Audio("no.wav");
 
 var haslo1 = "";
 
@@ -85,7 +89,7 @@ String.prototype.ustawZnak = function(miejsce, znak)
 
 	function sprawdz(nr)
 	{
-		
+		var trafiona = false;
 		
 		for(i=0; i<dlugosc; i++)
 		{
@@ -94,7 +98,47 @@ String.prototype.ustawZnak = function(miejsce, znak)
 				/*alert(i);*/
 				/*haslo1.charAt(i) = litery[nr]; taki zapis wywali error: invalid left hand side cośtam assigment w konsoli, funkcja charAt w tej postaci dokonuje odczytu a nie zapisu, potrzebna nowa funkcja stworzona od zera: patrz String.prototype... */
 				haslo1 = haslo1.ustawZnak(i,litery[nr]);
+				
+				trafiona = true;
 			}
 		}
-		wypisz_haslo();
+		/*wypisz_haslo(); funkcję przenosimy do if true, bo tylko wtedy powinna pojawić się tam trafiona litera*/
+		
+		if (trafiona ==true)
+		{
+			yes.play()
+			
+			var element = "lit" + nr; /*to daje nam id klikniętego diva z literą */
+			document.getElementById(element).style.background = "#003300"; /*zmiana koloru klikniętego diva */
+			document.getElementById(element).style.color = "#00c000"; /*zmiana koloru czcionki klikniętego diva */
+			document.getElementById(element).style.border = "3px solid #00c000"; /*zmiana koloru border klikniętego diva */
+			document.getElementById(element).style.cursor = "default"; /*zmiana wskaźnika na domyślną strzałkę klikniętego diva */
+			wypisz_haslo();
+		}
+		else
+		{
+			no.play()
+			
+			var element = "lit" + nr;
+			document.getElementById(element).style.background = "#330000"; 
+			document.getElementById(element).style.color = "#c00000"; 
+			document.getElementById(element).style.border = "3px solid #c00000"; 
+			document.getElementById(element).style.cursor = "default";
+			
+			document.getElementById(element).setAttribute("onclick",";"); //zmiana atrubutu z onclicka na pustą instrukcję
+			
+			//skucha
+			ile_skuch++;
+			var obraz = "img/s"+ ile_skuch + ".jpg";
+			document.getElementById("szubienica").innerHTML = 	'<img src="'+obraz+'" alt="" />';	 
+		}
+		
+		//wygrana
+		if(haslo == haslo1)
+			document.getElementById("alfabet").innerHTML = "Tak jest! Podano prawidłowe hasło: "+haslo+'<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+		
+		//porażka
+		if(ile_skuch>=9)
+			document.getElementById("alfabet").innerHTML = "Przegrana! Prawidłowe hasło: "+haslo+'<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+		
 	}
