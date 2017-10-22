df2d434138f680657b318e2172e23e7f4f2d6dd2
LukaszStusio <lukasz.stusio@gmail.com>
2017-10-22 11:31:44 +0200

    modulo, alphabet in JS

--- szubienica.js
+++ szubienica.js
@@ -19,4 +19,19 @@ function wypisz_haslo()
 	document.getElementById("plansza").innerHTML = haslo1;
 }
 
-window.onload = wypisz_haslo;
+window.onload = start;
+
+function start()
+{
+	var tresc_diva = "";
+	
+	for (i=0; i<=34; i++)
+	{
+		tresc_diva = tresc_diva + '<div class="litera">A</div>';
+		if (i+1 % 7 ==0) /*operator modulo %, reszta z dzielenia przez 7=0 */tresc_diva = tresc_diva + '<div style="clear:both;"></div>';
+	}
+	
+	document.getElementById("alfabet").innerHTML = tresc_diva;
+	
+	wypisz_haslo();
+}
